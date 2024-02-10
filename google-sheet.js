4005baf44fa48520ceadc127b4ed76ed4731aa49
theputras <theputras.media@gmail.com>
2024-02-10 18:15:37 +0700

    Update google-sheet.js

--- google-sheet.js
+++ google-sheet.js
@@ -1,9 +1,10 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwYQEcNueSfDRTWfehHvmsfvuce14CZFW3NmhAyMUgspuy-5bbgRGPdpdt6rjKiYGCT7Q/exec'
 const form = document.forms['submit-to-google-sheet']
+const url = '/daisekoi/output.html'
 
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => window.top.location.href='./daisekoi/output.html')
+    .then(response => window.location.replace("${url}"))
     .catch(error => console.error('Error!', error.message))
 })
