968f114afaa179a1a988361a17b75d9d8cda2d80
theputras <theputras.media@gmail.com>
2024-02-10 18:02:54 +0700

    Update

---
+++ google-sheet.js
@@ -0,0 +1,9 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwYQEcNueSfDRTWfehHvmsfvuce14CZFW3NmhAyMUgspuy-5bbgRGPdpdt6rjKiYGCT7Q/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => window.top.location.href='/output.html')
+    .catch(error => console.error('Error!', error.message))
+})
